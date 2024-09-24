73110073f65f80fa70085991b548c88a3a40d104
Vivek <vs690799@gmail.com>
2024-09-25 00:27:15 +0530

    added code to day20

--- day20/main.go
+++ day20/main.go
@@ -4,17 +4,40 @@ import "fmt"
 
 func findSecondLargestNum(s []int) interface{} {
 	max := 0
+	secondMax := 0
 	for _, v := range s {
 		if v > max {
+			secondMax = max
 			max = v
+		} else if v > secondMax && secondMax != max && v != max {
+			secondMax = v
+
 		}
 	}
-	return max
-	// return "Not enough unique elements"
+	if secondMax == 0 {
+		return "Not enough unique elements"
+	}
+	return secondMax
 }
 
 func main() {
 	numbers := []int{10, 20, 30, 40, 50}
+	numbers1 := []int{8, 3, 6, 3, 2}
+	numbers2 := []int{5, 5, 5, 5}
+	scores := []int{85, 92, 78, 92, 88}
+	scores1 := []int{100, 100, 100}
+	heights := []int{150, 200, 250, 200, 300}
+	heights1 := []int{120, 120, 120}
+	times := []int{12, 15, 10, 10}
+	times1 := []int{8, 8, 8}
 
 	fmt.Println(findSecondLargestNum(numbers))
+	fmt.Println(findSecondLargestNum(numbers1))
+	fmt.Println(findSecondLargestNum(numbers2))
+	fmt.Println(findSecondLargestNum(scores))
+	fmt.Println(findSecondLargestNum(scores1))
+	fmt.Println(findSecondLargestNum(heights))
+	fmt.Println(findSecondLargestNum(heights1))
+	fmt.Println(findSecondLargestNum(times))
+	fmt.Println(findSecondLargestNum(times1))
 }
