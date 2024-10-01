5a1317086dc3f9a454c4b55ccc5042488f47516d
Vivek <vs690799@gmail.com>
2024-10-02 00:17:15 +0530

    updated day9 code

--- neetcode/day9/main.go
+++ neetcode/day9/main.go
@@ -2,27 +2,28 @@ package main
 
 import (
 	"fmt"
-	"slices"
 )
 
 func longestConsecutive(nums []int) int {
 
-	res := []int{}
+	res := map[int]int{}
 
 	for _, v := range nums {
-		if !slices.Contains(res, v) {
-			res = append(res, v)
+		if _, ok := res[v]; !ok {
+			res[v] = v
 
 		}
 
 	}
 	longestseq := 0
 	resmap := make(map[int]int)
-
 	for _, v := range res {
-		if !slices.Contains(res, v-1) {
+		if _, ok := res[v-1]; !ok {
 			length := 0
-			for slices.Contains(res, v+length) {
+			for {
+				if _, ok := res[v+length]; !ok {
+					break
+				}
 				length++
 
 			}
@@ -44,6 +45,7 @@ func main() {
 	input1 := []int{0, 3, 2, 5, 4, 6, 1, 1}
 	input2 := []int{0}
 	input3 := []int{9, 1, 4, 7, 3, -1, 0, 5, 8, -1, 6}
+
 	fmt.Println(longestConsecutive(input))
 	fmt.Println(longestConsecutive(input1))
 	fmt.Println(longestConsecutive(input2))
