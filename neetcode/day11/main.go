659f248daf272966395417c5c323d0cd4b7dc153
Vivek <vs690799@gmail.com>
2024-10-03 13:19:06 +0530

    updated code

--- neetcode/day11/main.go
+++ neetcode/day11/main.go
@@ -3,20 +3,21 @@ package main
 import "fmt"
 
 func twoSum(n []int, target int) []int {
-	diff := 0
-	for i, v := range n {
-		diff = target - v
-		for j, v1 := range n {
-			if i == j {
-				continue
 
-			}
-			if diff == v1 {
-				return []int{i + 1, j + 1}
-			}
+	i, j := 0, len(n)-1
+
+	for i < j {
+		if n[i]+n[j] > target {
+			j--
+
+		} else if n[i]+n[j] < target {
+			i++
+
+		} else {
+			return []int{i + 1, j + 1}
 		}
-
 	}
+
 	return nil
 
 }
