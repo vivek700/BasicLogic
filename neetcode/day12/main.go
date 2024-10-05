88f80dab7d40292901ea5e34369a20c642b990ed
Vivek <vs690799@gmail.com>
2024-10-05 22:17:36 +0530

    added code

--- neetcode/day12/main.go
+++ neetcode/day12/main.go
@@ -1,5 +1,30 @@
 package main
 
+import "fmt"
+
+func threeSum(n []int) [][]int {
+	var newNum [][]int
+
+	l, r := 0, len(n)-1
+
+	for l < r {
+		for k := range n {
+			if k != l && k != r {
+				if n[l]+n[r]+n[k] == 0 {
+					newNum = append(newNum, []int{n[l], n[r], n[k]})
+				} else {
+					l++
+					r--
+				}
+			}
+		}
+	}
+	return newNum
+}
+
 func main() {
+	nums := []int{-1, 0, 1, 2, -1, -4}
+
+	fmt.Println(threeSum(nums))
 
 }
