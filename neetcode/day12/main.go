6a58dd22583a44c901a7f82d6e53f69d6a4f7dca
Vivek <vs690799@gmail.com>
2024-10-07 23:50:34 +0530

    updated code

--- neetcode/day12/main.go
+++ neetcode/day12/main.go
@@ -1,30 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func threeSum(n []int) [][]int {
 	var newNum [][]int
 
-	l, r := 0, len(n)-1
+	l, r := 0, 1
 
-	for l < r {
+	for r < len(n) {
 		for k := range n {
 			if k != l && k != r {
 				if n[l]+n[r]+n[k] == 0 {
 					newNum = append(newNum, []int{n[l], n[r], n[k]})
-				} else {
-					l++
-					r--
+
 				}
 			}
 		}
+		r++
+
 	}
+	for _, v := range newNum {
+		slices.Sort(v)
+	}
+
 	return newNum
 }
 
 func main() {
 	nums := []int{-1, 0, 1, 2, -1, -4}
+	nums1 := []int{0, 1, 1}
 
 	fmt.Println(threeSum(nums))
+	fmt.Println(threeSum(nums1))
 
 }
