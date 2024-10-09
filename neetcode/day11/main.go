b5d6085d00812a4f936813516860e5f8eb782b9e
Vivek <vs690799@gmail.com>
2024-10-09 23:17:40 +0530

    added day13

--- neetcode/day11/main.go
+++ neetcode/day11/main.go
@@ -25,6 +25,8 @@ func twoSum(n []int, target int) []int {
 func main() {
 
 	nums := []int{1, 2, 3, 4}
+	nums1 := []int{1, 2, 3, 4, 5, 6}
 	fmt.Println(twoSum(nums, 3))
+	fmt.Println(twoSum(nums1, 10))
 
 }
