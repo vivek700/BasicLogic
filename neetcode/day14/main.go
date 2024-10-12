6d7a204d0197e87218fc434202b379676503bd48
Vivek <vs690799@gmail.com>
2024-10-13 02:06:20 +0530

    updated code

--- neetcode/day14/main.go
+++ neetcode/day14/main.go
@@ -3,11 +3,39 @@ package main
 import "fmt"
 
 func trap(heights []int) int {
-	return 0
+	l := 0
+	trapWater := 0
+
+	for l < len(heights) {
+		if heights[l] > 0 {
+			r := l + 1
+			h := 0
+			for r < len(heights) {
+				if heights[l] > heights[r] {
+					h += heights[r]
+					r++
+				} else if r-l == 1 {
+					break
+
+				} else {
+					trapWater += heights[l]*(r-l-1) - h
+					break
+				}
+			}
+			l = r
+
+		} else {
+			l++
+		}
+
+	}
+	return trapWater
 }
 
 func main() {
 	heights := []int{0, 2, 0, 3, 1, 0, 1, 3, 2, 1}
+	heights1 := []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}
 	fmt.Println(trap(heights))
+	fmt.Println(trap(heights1))
 
 }
