77405cd5ee3303bd5805975f98bda4565d759fa1
Vivek <vs690799@gmail.com>
2024-10-14 12:21:25 +0530

    updated code

--- neetcode/day14/main.go
+++ neetcode/day14/main.go
@@ -7,32 +7,31 @@ import (
 func trap(height []int) int {
 	trapWater := 0
 	l, r := 0, len(height)-1
-	maxL, maxR := 0, height[r]
+	maxL, maxR := height[l], height[r]
 
 	for l < r {
 		if maxL < maxR {
+			l++
+			if height[l] > maxL {
+				maxL = height[l]
+			}
 			waterDrop := maxL - height[l]
 			if waterDrop > 0 {
 				trapWater += waterDrop
 
 			}
-			maxL = height[l]
-			fmt.Println("maxL:", maxL)
 
 		} else {
-			waterDrop := maxR - height[l]
+			r--
+			if height[r] > maxR {
+				maxR = height[r]
+			}
+			waterDrop := maxR - height[r]
 			if waterDrop > 0 {
 				trapWater += waterDrop
 
 			}
-			maxR = height[r]
-			fmt.Println("maxR:", maxR)
 
-		}
-		if maxL > maxR {
-			r--
-		} else {
-			l++
 		}
 
 	}
@@ -42,8 +41,10 @@ func trap(height []int) int {
 
 func main() {
 	heights := []int{0, 2, 0, 3, 1, 0, 1, 3, 2, 1}
-	// heights1 := []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}
+	heights1 := []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}
+	heights2 := []int{4, 2, 0, 3, 2, 5}
 	fmt.Println(trap(heights))
-	// fmt.Println(trap(heights1))
+	fmt.Println(trap(heights1))
+	fmt.Println(trap(heights2))
 
 }
