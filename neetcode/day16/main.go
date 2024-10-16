8fe2c344163a080e1da8beebd8e406ff5d9d6164
Vivek <vs690799@gmail.com>
2024-10-16 21:06:35 +0530

    updated code

--- neetcode/day16/main.go
+++ neetcode/day16/main.go
@@ -5,12 +5,15 @@ import (
 )
 
 func lengthOfLongestSubstring(str string) int {
+	// if len(str) == 1 {
+	// 	return 1
+	// }
 	maxLen := 0
 	start := 0
 	charIndex := make(map[rune]int)
 
 	for index, char := range str {
-		if value := charIndex[char]; value >= start {
+		if value, ok := charIndex[char]; ok && value >= start {
 			start = charIndex[char] + 1
 		} else {
 			maxLen = max(maxLen, index-start+1)
@@ -29,11 +32,17 @@ func main() {
 	s3 := "pwwkew"
 	s4 := "abcabcbb"
 	s5 := "dvdf"
+	s6 := " "
+	s7 := "au"
+	s8 := ""
 
 	fmt.Println(lengthOfLongestSubstring(s))
 	fmt.Println(lengthOfLongestSubstring(s1))
 	fmt.Println(lengthOfLongestSubstring(s3))
 	fmt.Println(lengthOfLongestSubstring(s4))
 	fmt.Println(lengthOfLongestSubstring(s5))
+	fmt.Println(lengthOfLongestSubstring(s6))
+	fmt.Println(lengthOfLongestSubstring(s7))
+	fmt.Println(lengthOfLongestSubstring(s8))
 
 }
