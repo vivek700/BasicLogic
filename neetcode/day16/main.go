37fa1bd5affc176e4d0af543c66615dfeeff73c5
Vivek <vs690799@gmail.com>
2024-10-16 13:26:31 +0530

    updated code

--- neetcode/day16/main.go
+++ neetcode/day16/main.go
@@ -2,45 +2,36 @@ package main
 
 import (
 	"fmt"
-	"slices"
 )
 
 func lengthOfLongestSubstring(str string) int {
 	maxLen := 0
-	l := 0
-	charset := []string{}
+	start := 0
+	charIndex := make(map[rune]int)
 
-	for r, v := range str {
-		k := 0
-		for k < len(charset) {
-			if charset[k] != string(v) {
-				k++
-			} else {
-				charset = slices.Delete(charset, l, l+1)
-				l++
-				break
-			}
+	for index, char := range str {
+		if value := charIndex[char]; value >= start {
+			start = charIndex[char] + 1
+		} else {
+			maxLen = max(maxLen, index-start+1)
 		}
-		charset = append(charset, string(v))
+		charIndex[char] = index
 
-		maxLen = max(maxLen, r-l+1)
 	}
-	// fmt.Println(maxLen)
-	// return len(charset)
-	fmt.Println(charset)
+
 	return maxLen
 
 }
 
 func main() {
 	s := "zxyzxyz"
-	// s1 := "xxxx"
+	s1 := "xxxx"
 	s3 := "pwwkew"
 	s4 := "abcabcbb"
 	s5 := "dvdf"
 
 	fmt.Println(lengthOfLongestSubstring(s))
-	// fmt.Println(lengthOfLongestSubstring(s1))
+	fmt.Println(lengthOfLongestSubstring(s1))
 	fmt.Println(lengthOfLongestSubstring(s3))
 	fmt.Println(lengthOfLongestSubstring(s4))
 	fmt.Println(lengthOfLongestSubstring(s5))
