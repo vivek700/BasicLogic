7024d7abaecf3587e1e71e34a9309551b9d55d27
Vivek <vs690799@gmail.com>
2024-10-17 21:25:51 +0530

    updated code

--- neetcode/day17/main.go
+++ neetcode/day17/main.go
@@ -1,5 +1,38 @@
 package main
 
+import "fmt"
+
+func characterReplacement(str string, k int) int {
+	maxLen := 0
+
+	l, r := 0, 1
+
+	for r < len(str) {
+		if str[l] != str[r] {
+			if k > 0 {
+				k--
+			} else {
+				l = r
+			}
+
+		}
+		maxLen = max(maxLen, r-l+1)
+		r++
+
+	}
+	return maxLen
+
+}
+
 func main() {
+	s := "XYYX"
+	s1 := "AAABABB"
+	s2 := "XXXX"
+	s3 := "ABBB"
+
+	fmt.Println(characterReplacement(s, 2))
+	fmt.Println(characterReplacement(s1, 1))
+	fmt.Println(characterReplacement(s2, 1))
+	fmt.Println(characterReplacement(s3, 2))
 
 }
