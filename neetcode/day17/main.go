4c3e65eef4e9c0a9f39ef2e5004d62de38b91372
Vivek <vs690799@gmail.com>
2024-10-18 13:50:49 +0530

    updated code

--- neetcode/day17/main.go
+++ neetcode/day17/main.go
@@ -4,22 +4,26 @@ import "fmt"
 
 func characterReplacement(str string, k int) int {
 	maxLen := 0
+	counter := 0
+	l := 0
 
-	l, r := 0, 1
-
-	for r < len(str) {
-		if str[l] != str[r] {
-			if k > 0 {
-				k--
-			} else {
-				l = r
-			}
-
+	count := make(map[rune]int)
+	for r, v := range str {
+		count[v] = 1 + count[v]
+		if counter < count[rune(str[l])] {
+			counter = count[rune(str[l])]
+		} else if counter < count[rune(str[r])] {
+			counter = count[rune(str[r])]
 		}
-		maxLen = max(maxLen, r-l+1)
-		r++
+		if r-l+1-counter > k {
+			counter--
+			l++
+		}
+		maxLen = max(len(count), r-l+1)
 
 	}
+	fmt.Println(count)
+
 	return maxLen
 
 }
@@ -29,10 +33,14 @@ func main() {
 	s1 := "AAABABB"
 	s2 := "XXXX"
 	s3 := "ABBB"
+	s4 := "AABABBA"
+	s5 := "AABA"
 
 	fmt.Println(characterReplacement(s, 2))
 	fmt.Println(characterReplacement(s1, 1))
 	fmt.Println(characterReplacement(s2, 1))
 	fmt.Println(characterReplacement(s3, 2))
+	fmt.Println(characterReplacement(s4, 1))
+	fmt.Println(characterReplacement(s5, 0))
 
 }
