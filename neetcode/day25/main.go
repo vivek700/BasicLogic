5b094411a3378d59fafcaf798276ed7b1d25b651
Vivek <vs690799@gmail.com>
2024-11-04 21:39:54 +0530

    added day25

---
+++ neetcode/day25/main.go
@@ -0,0 +1,21 @@
+package main
+
+type stack struct {
+	item [][]int
+}
+
+func (s *stack) Pop() (int, int) {
+	s.item
+
+}
+
+
+func dailyTemperatures(temperatures []int) []int {
+
+	res := make([]int,len(temperatures))
+	stack := []
+}
+
+func main() {
+
+}
