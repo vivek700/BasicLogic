333ab56eae3407f349cf8e73a5591889afbc9e04
Vivek <vs690799@gmail.com>
2024-11-14 23:56:57 +0530

    added day27

---
+++ neetcode/day27/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type Pair struct {
+	index  int
+	height int
+}
+
+func largestRectangleArea(heights []int) int {
+	maxArea := 0
+	stack := []Pair{}
+
+	for i, h := range heights {
+		start := i
+		for len(stack) > 0 && stack[len(stack)-1].height > h {
+			index := stack[len(stack)-1].index
+			height := stack[len(stack)-1].height
+			stack = stack[:len(stack)-1]
+			maxArea = max(height * (i - index))
+
+			start = index
+
+		}
+		stack = append(stack, Pair{index: start, height: h})
+	}
+	for _, p := range stack {
+		maxArea = max(p.height * (len(heights) - p.index))
+	}
+	return maxArea
+
+}
+
+func main() {
+
+	heights := []int{7, 1, 7, 2, 2, 4}
+
+	fmt.Println(largestRectangleArea(heights))
+}
