2225c1387a8908231ae772e20e5eda3a25afd97e
Vivek <vs690799@gmail.com>
2024-11-29 00:06:03 +0530

    fixed bug

--- neetcode/day29/main.go
+++ neetcode/day29/main.go
@@ -6,12 +6,12 @@ func search(nums []int, target int) bool {
 	l, r := 0, len(nums)-1
 	for l <= r {
 		mid := l + (r-l)/2
-		if nums[mid] > target {
-			l = mid + 1
-		} else if nums[mid] < target {
-			r = mid - 1
-		} else {
+		if nums[mid] == target {
 			return true
+		} else if nums[mid] < target {
+			l = mid + 1
+		} else {
+			r = mid - 1
 		}
 	}
 	return false
@@ -27,6 +27,6 @@ func searchMatrix(matrix [][]int, target int) bool {
 func main() {
 	matrix := [][]int{{1, 2, 4, 8}, {10, 11, 12, 13}, {14, 20, 30, 40}}
 	fmt.Println(searchMatrix(matrix, 10))
-	fmt.Println(search([]int{1, 2, 3, 4}, 3))
+	fmt.Println(search([]int{1, 2, 3, 4, 5, 6, 8, 9, 20}, 8))
 
 }
